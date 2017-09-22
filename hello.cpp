60427d3f60ed1a33b9360debe2b4001c0ad61e40
jjcurry <32207126+jjcurry@users.noreply.github.com>
2017-09-22 14:15:04 -0700

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -6,4 +6,7 @@ int main()
 {
 	cout << "Hello!" <<  endl;
 	cout << "Great, you managed to get a copy of the repository from GitHub" <<  endl;
+	// here is an addition from jjcurry
+	cout << "My name is Jesse and this is an addition to hello.cpp" << endl;
+	
 }
